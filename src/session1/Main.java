bd3e7790c08edde426a2e669c31814db60293c83
Vndark5464 <nguyenhuulinh5464@gmail.com>
2022-10-11 10:45:17 +0700

    Java

---
+++ src/session1/Main.java
@@ -0,0 +1,10 @@
+package session1;
+
+public class Main {
+    public static void main(String[] args){
+        Student s = new Student();
+        s.name = "Nam";
+        s.age = 18;
+        
+    }
+}
---
+++ src/session1/Student.java
@@ -0,0 +1,7 @@
+package session1;
+
+public class Student {
+    String name;
+    int age;
+
+}
