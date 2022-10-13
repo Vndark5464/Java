57f92131d74c5d69f4333c42048424c95f7e2260
Vndark5464 <nguyenhuulinh5464@gmail.com>
2022-10-13 11:03:01 +0700

    java

--- src/session1/Student.java
+++ src/session1/Student.java
@@ -1,12 +1,11 @@
 package session1;
 
 public class Student {
-    String name;
-    int age = 28 ;
+    public String name;
+    int age = 20;
 
     public void showInfo(){
-        System.out.println("Name"+name);
-        System.out.println("Age"+age);
+        System.out.println("Name:"+name);
+        System.out.println("Age:"+age);
     }
-
 }
