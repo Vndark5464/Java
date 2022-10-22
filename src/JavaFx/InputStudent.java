7dc12fc45c2b497e2c3704483ad9b6818393abc0
Vndark5464 <nguyenhuulinh5464@gmail.com>
2022-10-22 08:08:15 +0700

    java

---
+++ src/JavaFx/InputStudent.java
@@ -0,0 +1,32 @@
+package JavaFx;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class InputStudent {
+    public TextField txtName;
+    public TextField txtEmail;
+    public TextField txtAddress;
+    public TextField txtAge;
+    public TextField txtTel;
+    public Text strName;
+    public Text strEmail;
+    public Text strAddress;
+    public Text strAge;
+    public Text strTel;
+
+    public void submit(ActionEvent actionEvent) {
+        String name = txtName.getText();
+        String email = txtEmail.getText();
+        String address = txtAddress.getText();
+        int age = Integer.parseInt(txtAge.getText());
+        String tel = txtTel.getText();
+
+        strName.setText(name);
+        strEmail.setText(email);
+        strAddress.setText(address);
+        strAge.setText(Integer.toString(age));
+        strTel.setText(tel);
+    }
+}
